d19f1106a45d5b08a8dbcef1c1d25df720ab2526
mikolyants1 <y.mikolyants@mail.ru>
2024-04-20 07:58:07 +0300

    fix connect

--- client/src/components/api/apiClient.tsx
+++ client/src/components/api/apiClient.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
 export const apiClient:AxiosInstance = axios.create({
-    baseURL:"http://localhost:5000/",
+    baseURL:"http://localhost:5000/api",
 })
