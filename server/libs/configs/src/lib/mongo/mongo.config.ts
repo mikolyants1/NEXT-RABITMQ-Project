273e9790b37e4f29723d23fe98d031fde6858753
mikolyants1 <y.mikolyants@mail.ru>
2024-04-30 22:39:55 +0300

    update mongo config

--- server/libs/configs/src/lib/mongo/mongo.config.ts
+++ server/libs/configs/src/lib/mongo/mongo.config.ts
@@ -5,9 +5,12 @@ export const MongoConfig = ():MongooseModuleAsyncOptions => ({
   imports:[ConfigModule],
   inject:[ConfigService],
   useFactory:(service:ConfigService)=>{
+    const login = service.get<string>("MONGO_LOGIN");
+    const pass = service.get<string>("MONGO_PASSWORD");
+    const host = service.get<string>("MONGO_HOST");
+    const port = service.get<string>("MONGO_PORT");
+
     return {
-      uri:service.get("MONGO_URI")
+      uri:`mongodb://${login}:${pass}@${host}:${port}/`
   }}
-})
-
-
+});
